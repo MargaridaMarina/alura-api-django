3c4dae31e78adb45fe95aab7a7f2b2ff5aef1458
Margarida <margaridammcm@gmail.com>
2022-05-14 19:40:24 -0300

    Filtros, buscas e paginação da página do admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,5 +4,9 @@ from .models import Receita
 class ListandoReceitas(admin.ModelAdmin):
   list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'rendimento', 'ingredientes')
   list_display_links = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'rendimento', 'ingredientes')
+  search_fields = ('nome_receita', 'categoria', 'ingredientes')
+  list_filter = ('ingredientes',)
+  list_per_page = 2
+
 
 admin.site.register(Receita, ListandoReceitas)
