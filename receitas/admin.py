e41fd79030f7f1ba732a88ab5c9d54972f487461
Margarida <margaridammcm@gmail.com>
2022-05-15 18:55:47 -0300

    Editando receitas que serão exibidas via admin

--- receitas/admin.py
+++ receitas/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from .models import Receita
 
 class ListandoReceitas(admin.ModelAdmin):
-  list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'rendimento', 'ingredientes')
+  list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'rendimento', 'ingredientes', 'publicada')
   list_display_links = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'rendimento', 'ingredientes')
   search_fields = ('nome_receita', 'categoria', 'ingredientes')
   list_filter = ('ingredientes',)
+  list_editable = ('publicada',)
   list_per_page = 2
 
 
